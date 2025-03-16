be9a211e312d2a2c4bf5126260dc191768390cc2
Christopher Lee <crlapples19@gmail.com>
2025-03-16 15:26:05 -0700

    a

--- myApp.js
+++ myApp.js
@@ -5,8 +5,10 @@ const absolutePath = __dirname + '/views/index.html';
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 });
-app.use(express.static(__dirname + "/public"));
-
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res) {
+  res.json({ "message": "Hello json" })
+});
 
 
 
