3c8ddad900ab7b8b3fa89cf5f8a5c2cbc9a3db3d
Christopher Lee <crlapples19@gmail.com>
2025-03-16 21:09:04 -0700

    a

--- myApp.js
+++ myApp.js
@@ -31,6 +31,8 @@ app.get("/:word/echo", function(req, res) {
 })
 app.route("/name").get(function(req, res) {
   res.json({ name: `${req.query.first} ${req.query.last}` });
+}).post("/name", function(req, res) {
+  res.json({ name: `${req.body.first} ${req.body.last}`});
 });
 
 
